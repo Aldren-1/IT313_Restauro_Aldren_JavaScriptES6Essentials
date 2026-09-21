745d15d462b27ac17c20e3f259e2d879a67da752
Aldren-1 <restauroaldrin@gmail.com>
2026-09-21 22:52:45 +0800

    lab2

---
+++ gradeUtils.js
@@ -0,0 +1,8 @@
+export function computeAverage(prelim, midterm,final){
+    return(prelim + midterm + final) / 3;
+}
+
+export default function isPassing(average) {
+    return average >=75;
+    
+}
